3cd3fe57d3e1641165142e1c9df11144b7491bbe
Cheurfi Belhadj Yassine <91964469+yaCHBn2004@users.noreply.github.com>
2024-01-25 10:13:46 +0000

    Add files via upload

---
+++ BG.tsx
@@ -0,0 +1,28 @@
+// import About from "./About";
+// import About2 from "./About2";
+// import HomeA from "./HomeA";
+// import HomeB from "./HomeB";
+// import Login from "./Login";
+// import Navbar from "./Navbar";
+import l from "../img/bg.svg";
+
+
+function BG() {
+ 
+   
+    return (
+
+     <div className="relative w-full h-auto ">
+        <img src={l}  className="w-full relative "/>
+        <div className=" z-20 absolute flex flex-col top-[20%] gap-3 w-[90%] left-[8%] ">
+           <p className="flex flex-row gap-2 text-3xl "> <p className="text-[#fff] f6">Explorez notre site annuaire dédié</p><p className="text-[#3498DB] f8"> aux avocats en Algérie,</p></p>
+            <p className="f33 text-[#fff] text-2xl ">
+un outil essentiel pour vous connecter avec des professionnels du droit qualifiés. Trouvez facilement des avocats spécialisés dans divers domaines juridiques, de la famille au droit des affaires.</p>
+            <p className="f33 text-[#fff] text-base"> Notre plateforme vous offre des profils détaillés, des informations de contact, et la possibilité de filtrer les résultats en fonction de vos besoins spécifiques. Simplifiez votre recherche d'experts juridiques en utilisant notre annuaire, un compagnon fiable pour vos préoccupations légales en Algérie</p>
+        </div>
+     </div>
+    );
+  }
+  
+  export default BG;
+  
---
+++ Navbar.tsx
@@ -0,0 +1,50 @@
+import logo from "../img/logo.svg";
+import search from "../img/search.svg";
+import { useNavigate } from "react-router-dom";
+
+
+
+import  { useState } from 'react';
+import "../index.css";
+
+
+
+function Navbar() {
+  const navigate = useNavigate();
+
+
+  const [isExpanded, setExpanded] = useState(false);
+
+
+
+  return (
+    <div className="relative h-[8vh] w-full z-50 ">
+      <div className=" flex flex-row justify-between fixed h-[8vh] w-full bg-[#31404A] opacity-75 p-[1%]  /*backdrop ${isExpanded ? 'blurred' : ''}*/ ">
+        <img src={logo} className="w-[5%]" />
+        <div className="f1 flex flex-row justify-between text-[#FFFFFF] w-[50%]">
+          <a href="">Home</a>
+          <a href="">About</a>
+          <a href="">Avocats</a>
+          <a href="">Annuaire</a>
+          <a href="">Help</a>
+          <a href="">Contact</a>
+        </div>
+        <button
+          onClick={() => {
+            console.log('Before toggle:', isExpanded);
+            navigate("/Search");
+
+            setExpanded(!isExpanded); //  ceci c pour changer l'état d'expansion
+            console.log('After toggle:', !isExpanded);
+
+          }}
+          className="w-[2%] ${isExpanded ? 'hover:blur focus:blur' : ''}"
+        >
+          <img src={search} />
+        </button>
+      </div>
+    </div>
+  );
+}
+
+export default Navbar;
